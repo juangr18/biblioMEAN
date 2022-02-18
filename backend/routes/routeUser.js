5899fc864065d7b12d3da2e2785b1a069850f7b5
juangr18 <juanr970318@gmail.com>
2022-02-18 08:46:43 -0500

    Add code line in userRoutes.js

--- backend/routes/routeUser.js
+++ backend/routes/routeUser.js
@@ -1,8 +1,8 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-
+import userValidate from "../middlewares/userValidate.js"
 const router = express.Router();
 //http://localhost:4000/api/user/registerUser
-router.post("/registerUser", userController.registerUser);
+router.post("/registerUser", userValidate.existUser, userController.registerUser);
 
 export default router;

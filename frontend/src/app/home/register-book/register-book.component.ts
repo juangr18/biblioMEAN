09c73939c5aedf742086492b6f970cbfedbb12b1
juangr18 <juanr970318@gmail.com>
2022-03-06 23:54:34 -0500

    Implement code for forms and register book

---
+++ frontend/src/app/home/register-book/register-book.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from "../../services/task.service";
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-register-book',
+  templateUrl: './register-book.component.html',
+  styleUrls: ['./register-book.component.css']
+})
+export class RegisterBookComponent implements OnInit {
+
+  registerData: any;
+  message: string = '';
+  positionHorizontal: MatSnackBarHorizontalPosition = 'right';
+  positionVertical: MatSnackBarVerticalPosition = 'top';
+
+  constructor(
+    private _taskService: TaskService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registerData = {};
+  }
+
+  registerBook(){
+  if (
+    !this.registerData.isbn ||
+    !this.registerData.author ||
+    !this.registerData.name ||
+    !this.registerData.section ||
+    this.registerData.pages===0 ||
+    this.registerData.price===0
+  ) {
+    this.message = 'Incomplete data';
+    this.openSnackBarError();
+    this.registerData = {};
+  } else {
+    console.log(this.registerData);
+
+    this._taskService.registerBook(this.registerData).subscribe({
+      next: (v) => {
+        this._router.navigate(['/listBook']);
+        this.message = 'Successful register book';
+        this.openSnackBarSuccesful();
+      },
+      error: (err) => {
+        this.message = err.error.message;
+        this.openSnackBarError();
+      },
+    });
+  }
+}
+
+openSnackBarSuccesful() {
+  this._snackBar.open(this.message, 'X', {
+    horizontalPosition: this.positionHorizontal,
+    verticalPosition:this.positionVertical,
+    duration: 1500,
+    panelClass: ['snackBarSuccesful'],
+  });
+}
+
+openSnackBarError() {
+  this._snackBar.open(this.message, 'X', {
+    horizontalPosition: this.positionHorizontal,
+    verticalPosition:this.positionVertical,
+    duration: 1500,
+    panelClass: ['snackBarError'],
+  });
+}
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private env: string;
+
+  constructor(private _http: HttpClient) {
+    this.env = environment.APP_URL;
+  }
+
+  registerBook(book:any) {
+    return this._http.post<any>(this.env + 'book/registerBook', book);
+  }
+}
